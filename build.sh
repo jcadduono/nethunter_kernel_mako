6ebfd07746ad5959e90d9a70ac60cc7453e8251f
James Christopher Adduono <jc@adduono.com>
2016-01-09 18:46:23 -0500

    Add smarter build.sh and menuconfig.sh scripts for easy building

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# simple bash script for executing build
+
+RDIR=$(pwd)
+
+TOOLCHAIN=/home/jc/build/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06
+
+THREADS=5
+
+[ -z $VERSION ] && \
+# version number
+VERSION=$(cat $RDIR/VERSION)
+
+export ARCH=arm
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-cortex_a15-linux-gnueabihf-
+
+cd $RDIR
+
+[ "$1" ] && DEVICE=$1 || DEVICE=mako
+[ "$TARGET" ] || TARGET=nethunter
+
+DEFCONFIG=${TARGET}_${DEVICE}_defconfig
+FILE=arch/arm/configs/$DEFCONFIG
+
+[ -f "$FILE" ] || {
+	echo "Defconfig not found: $FILE"
+	exit 1
+}
+
+export LOCALVERSION=$TARGET-$DEVICE-$VERSION
+
+echo "Cleaning build..."
+rm -rf build && mkdir build
+
+make -C $RDIR O=build $DEFCONFIG &&
+echo "Starting build for $LOCALVERSION..." &&
+make -C $RDIR O=build -j"$THREADS" &&
+echo "Done building $LOCALVERSION."
